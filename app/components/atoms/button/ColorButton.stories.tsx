1c2bb5cb5a3b292a71e455c6609efe92f150dd59
ogo-js <taichi.ogo@japan-systems.co.jp>
2024-08-06 16:53:51 +0900

    chore: change ColorButton colors

--- app/components/atoms/button/ColorButton.stories.tsx
+++ app/components/atoms/button/ColorButton.stories.tsx
@@ -17,8 +17,23 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  name: "ColorButton",
+export const Colors: Story = {
+  render: () => {
+    return (
+      <>
+        <ColorButton colorScheme="gray">Button</ColorButton>
+        <ColorButton colorScheme="red">Button</ColorButton>
+        <ColorButton colorScheme="orange">Button</ColorButton>
+        <ColorButton colorScheme="yellow">Button</ColorButton>
+        <ColorButton colorScheme="green">Button</ColorButton>
+        <ColorButton colorScheme="teal">Button</ColorButton>
+        <ColorButton colorScheme="blue">Button</ColorButton>
+        <ColorButton colorScheme="cyan">Button</ColorButton>
+        <ColorButton colorScheme="purple">Button</ColorButton>
+        <ColorButton colorScheme="pink">Button</ColorButton>
+      </>
+    );
+  },
   args: {
     colorScheme: "gray",
   },
